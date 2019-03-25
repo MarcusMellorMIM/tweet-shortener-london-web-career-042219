77a80a7346b52c6e47ebdc3c55b4da739815d0a1
Marcus Mellor <marcus@mellor.com>
2019-03-25 12:17:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,7 +28,7 @@ def word_substituter( tweet )
 
 end
 
-def bulk_tweet_shortner
+def bulk_tweet_shortener
 
   puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
 
