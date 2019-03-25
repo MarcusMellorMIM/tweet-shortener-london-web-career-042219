6c68008156c013264ef42b176e2a89cdf6c5bb3a
Marcus Mellor <marcus@mellor.com>
2019-03-25 11:33:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def dictionary
+  dictionary_hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@".
+    "and" => "&"
+  }
+end
