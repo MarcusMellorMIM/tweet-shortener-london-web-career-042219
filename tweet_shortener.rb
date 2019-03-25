583fb3a4eadc645d2d52001383b68290364664a9
Marcus Mellor <marcus@mellor.com>
2019-03-25 12:16:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,16 +14,51 @@ end
 
 def word_substituter( tweet )
   
-  tweet_array = array.split(" ")
-  dictionary~_hash = dictionary
+  tweet_array = tweet.split(" ")
+  dictionary_hash = dictionary
   counter=0
+  replacement_word=""
   tweet_array.each do | word |
-    if replacement_word=dictionary[word]
+  if replacement_word=dictionary[word] #if true the key exists
       tweet_array[counter]=replacement_word  
-    end
+  end
     counter+=1
   end
   new_tweet=tweet_array.join(" ")
-  new__tweet
 
 end
+
+def bulk_tweet_shortner
+
+  puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+
+  puts word_substituter("OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?")
+
+  puts word_substituter("I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.")
+
+  puts word_substituter("GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!")
+
+  puts word_substituter("New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag")
+
+end
+
+def selective_tweet_shortener( tweet )
+  new_tweet=tweet
+  if tweet.length > 140
+    new_tweet=word_substituter( tweet )
+  end
+  new_tweet
+end
+
+def shortened_tweet_truncator( tweet )
+  new_tweet=tweet
+  if tweet.length > 140
+    new_tweet=word_substituter( tweet )
+    if new_tweet.length > 140
+    new_tweet=new_tweet[0..136]
+    new_tweet+="..."
+    end
+  end
+  new_tweet
+end
+
