257b62098c1ee0cc380ed0f36bcaa04f78127173
Marcus Mellor <marcus@mellor.com>
2019-03-25 12:21:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,17 +28,11 @@ def word_substituter( tweet )
 
 end
 
-def bulk_tweet_shortener
+def bulk_tweet_shortner( tweets )
 
-  puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
-
-  puts word_substituter("OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?")
-
-  puts word_substituter("I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.")
-
-  puts word_substituter("GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!")
-
-  puts word_substituter("New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag")
+  tweets.each do | tweet |
+    puts word_substituter(tweet)
+  end
 
 end
 
